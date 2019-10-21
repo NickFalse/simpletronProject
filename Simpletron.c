9f4d3915e698a9f6ef3c53f91778809114053d85
NickFalse <ntrue@smcm.edu>
2019-10-21 15:00:34 -0400

    Update Simpletron.c
    add defines

--- Simpletron.c
+++ Simpletron.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define READ 10
+#define WRITE 11
+#define LOAD 20
+#define STORE 21
+#define ADD 30
+#define SUBTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+#define BRANCH 40
+#define BRANCHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
 int arr[100];
 int accumulator = 0;
 int instructionCounter = 0;
